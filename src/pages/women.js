7fcb9b03ed7f84bfc485162850a1c4d501a5f059
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-01-26 18:06:56 +0500

    Women: Wrapper added

--- src/pages/women.js
+++ src/pages/women.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import {Wrapper, Hero, Slider, ImpossibleBanner} from "../components"
+
 export default class Women extends React.Component {
     render() {
         return (
-            <div>
-                Hello Women
-            </div>
+            <Wrapper name='Women'>
+                <div className='section-social-media'>
+                    <h2>--- Follow FittFind</h2>
+                    <Slider type='social_media' />
+                </div>
+                <ImpossibleBanner />
+            </Wrapper>
         );
     }
 }
