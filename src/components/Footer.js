fab74465d104dc507df025815c2b4324fbcbc76f
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-01-26 06:39:24 +0500

    Complete footer structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
-// import replaceable from '../images/replaceable.png'
 import  '../styles/footer.css'
+
+import logo_light from '../images/logo-dark.png'
+import replaceable from '../images/replaceable.png'
 
 const Footer = () => (
     <footer>
-        <div>
+        <div className='row-top'>
             <p>
                 <span>Sign up </span>
                 to get the latest on sales, <br/> new releases and more...
@@ -16,8 +18,56 @@ const Footer = () => (
                 get notified
             </Link>
         </div>
-        <div>
-            // write your stuff here
+        <div className='row-middle'>
+            <div className='col-media'>
+                <Link to={'/'}>
+                    <img src={logo_light} alt='logo_light' width={100} />
+                </Link>
+                <div className='social_media_links'>
+                    <Link to={'/'}>
+                        <img src={replaceable} alt='media_icon' width={100} />
+                    </Link>
+                    <Link to={'/'}>
+                        <img src={replaceable} alt='media_icon' width={100} />
+                    </Link>
+                    <Link to={'/'}>
+                        <img src={replaceable} alt='media_icon' width={100} />
+                    </Link>
+                    <Link to={'/'}>
+                        <img src={replaceable} alt='media_icon' width={100} />
+                    </Link>
+                </div>
+            </div>
+            <div className='col-menu'>
+                <div className='menu-row'>
+                    <h4>Shop</h4>
+                    <Link to={'/women'}>Women</Link>
+                    <Link to={'/men'}>Men</Link>
+                </div>
+                <div className='menu-row'>
+                    <h4>Products</h4>
+                    <Link to={'/'}>Clothing</Link>
+                    <Link to={'/'}>Accessories</Link>
+                    <Link to={'/'}>New Arrivals</Link>
+                </div>
+                <div className='menu-row'>
+                    <h4>Support</h4>
+                    <Link to={'/'}>Track my order</Link>
+                    <Link to={'/'}>Support</Link>
+                    <Link to={'/'}>Terms and Conditions</Link>
+                    <Link to={'/'}>Privacy Policy</Link>
+                </div>
+                <div className='menu-row'>
+                    <h4>Company</h4>
+                    <Link to={'/'}>About us</Link>
+                    <Link to={'/'}>Press</Link>
+                    <Link to={'/'}>Contact</Link>
+                </div>
+            </div>
+        </div>
+        <div className='row-bottom'>
+            <p>Copywrite 2020 Fittfind</p>
+            <p>Design <Link to={'/'}>www.jakobsze.com</Link></p>
         </div>
     </footer>
 );
