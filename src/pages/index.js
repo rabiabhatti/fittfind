7150833b61efd8c9026440e843f48396a6ae4566
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-01-26 05:52:29 +0500

    Home: apps section added

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,6 +62,20 @@ class IndexPage extends React.Component {
                             </div>
                         </div>
                     </div>
+                    <div className='section-apps'>
+                        <h2>Book a personal trainer and more with the <span>fittfind</span> app</h2>
+                        <div className='section-apps-left'>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. <br/> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <Link to={'/'}>
+                                <span>Find out more</span>
+                                <img src={forwardIcon} alt='forward_icon' />
+                            </Link>
+                        </div>
+                        <div className='section-apps-right'>
+                            <img src={women_banner} alt='app_store_imag' width={400} />
+                            <img src={women_banner} alt='app_store_video' width={400} />
+                        </div>
+                    </div>
                     <div className='section-features'>
                         <h2>--- Featured Items</h2>
                         <Slider type='products' />
