c29aba24d3e1fdcda2e1ba0bd7224a5d9c8dd698
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-01-28 03:43:00 +0500

    App: motivation-banner added

--- src/pages/app.js
+++ src/pages/app.js
@@ -5,6 +5,7 @@ import '../styles/application.css'
 import { Wrapper, Hero } from '../components'
 import phone_img from '../images/women_banner.jpg'
 import play_video from '../images/youtube_icon.png'
+import vertical_line from '../images/vertical_line.png'
 import horizontal_line from '../images/horizontal_line.png'
 
 export default class Application extends React.Component {
@@ -79,6 +80,14 @@ export default class Application extends React.Component {
                                 <img src={horizontal_line} alt='horizontal_line' />
                                 <p>It’s often a good idea to show which page is currently being viewed by visually changing the link matching the current page.</p>
                             </div>
+                        </div>
+                    </div>
+                    <div className='section-application-motivation-banner'>
+                        <img src={phone_img} alt='phone_img' />
+                        <div className='section-application-motivation-banner-dialogue'>
+                            <img src={vertical_line} alt='vertical_line' />
+                            <p>"The question isn't can you, it's will you?"</p>
+                            <img src={vertical_line} alt='vertical_line' />
                         </div>
                     </div>
                 </div>
