6431a04d1950b725b33542371068698fe95b2706
Rustam Bhatti <rustamiqbalbhatti@yahoo.com>
2020-02-01 16:21:25 +0500

    Gym Hero finished

--- src/pages/gym.js
+++ src/pages/gym.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import '../styles/gym.css'
 import { Wrapper, Hero } from '../components'
 import one from '../images/01.png'
 import two from '../images/02.png'
@@ -18,22 +19,29 @@ import guardian_logo from '../images/guardian.png'
 import techradar_logo from '../images/techradar.png'
 import huffington_post_logo from '../images/huffington-post.png'
 import evening_standard_logo from '../images/evening-standard.png'
-
+import slider_men from '../images/slider-men.png'
 export default class Gym extends React.Component{
     render() {
         return (
             <Wrapper name='Gym'>
                 <Hero className='section-hero-gym'>
-                    <h2>Flexible Gym access</h2>
-                    <div className='section-hero-gym-main-banner'>
-                        <h1>Find gym in</h1>
-                        <div className='section-hero-gym-main-banner-heading'>
-                            <span>London</span>
-                            <span>Enter location <br/>or postcode</span>
+                    <div className='section-hero-gym-slider'>
+                        <div className='section-hero-gym-img'>
+                            <img src={slider_men} alt='slider_men' />
                         </div>
-                        <div className='section-hero-gym-kickstarter-ref'>
-                            <p>coming soon on</p>
-                            <img src={kickstarter_logo} alt='kickstarter_logo' />
+                        <div className='section-hero-gym-text'>
+                            <h2>Flexible Gym access</h2>
+                            <div className='section-hero-gym-main-banner'>
+                                <h1>Find gym in</h1>
+                                <div className='section-hero-gym-main-banner-heading'>
+                                    <span>London</span>
+                                    <span>Enter location <br/>or postcode</span>
+                                </div>
+                                <div className='section-hero-gym-kickstarter-ref'>
+                                    <p>coming soon on</p>
+                                    <img src={kickstarter_logo} alt='kickstarter_logo' />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </Hero>
