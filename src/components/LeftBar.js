52db7c6ecad355adb781af0f831ee2a1e64ab8b8
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-02-15 00:23:46 +0500

    LeftBar: scrolling added

--- src/components/LeftBar.js
+++ src/components/LeftBar.js
@@ -6,7 +6,7 @@ import scrollDownIcon from '../images/scroll_down_icon.png'
 const LeftBar = () => (
     <div className='section-left-bar'>
         <p className='section-left-bar-text'>Find & release your inner athlete</p>
-        <button className='section-left-bar-btn'>
+        <button className='section-left-bar-btn' onClick={() =>  window.scrollTo(0, window.innerHeight)}>
             <img src={scrollDownIcon} alt='scrollDownIcon' />
             <span>scroll down</span>
         </button>
