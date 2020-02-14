78333fce4f09c7efb093afd31a14756829eeca77
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-02-15 00:27:21 +0500

    Create returns.js

---
+++ src/pages/contact/returns.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Wrapper} from "../../components";
+
+export default class Returns extends React.Component {
+    render() {
+        return (
+            <Wrapper name='Returns'>
+                <p>Returns</p>
+            </Wrapper>
+        )
+    }
+}
