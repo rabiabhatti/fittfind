414afb9500e88b1ccb294d0c63d08a8a115aa109
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-02-25 00:35:32 +0500

    Returns: banner added

--- src/pages/contact/returns.js
+++ src/pages/contact/returns.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import {Link} from "gatsby";
 
-import { Wrapper} from "../../components";
+import { Wrapper, Hero, ImpossibleBanner} from "../../components";
+import backward_icon from "../../images/backward-icon.png";
 
 export default class Returns extends React.Component {
     render() {
         return (
             <Wrapper name='Returns'>
-                <p>Returns</p>
+                <Hero>
+                    <div className='section-shipping-hero'>
+                        <Link to='/contact/'>
+                            <img src={backward_icon} alt='backward_icon' />
+                            <span>Back to contact</span>
+                        </Link>
+                        <h1>Returns</h1>
+                    </div>
+                </Hero>
+                <ImpossibleBanner />
             </Wrapper>
         )
     }
