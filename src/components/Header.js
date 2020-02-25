b6297ff3b6f818d69ec0be5cfafeacccffa47304
Rustam Bhatti <rustamiqbalbhatti@yahoo.com>
2020-02-25 19:53:34 +0500

    Home Page Bug fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -112,14 +112,14 @@ class Header extends React.Component {
                                 <span>USD</span>
                             </span>
                             <Link to="/basket"  className='section-nav-right-cart'>
-                                <img src={cart} alt='Cart icon' />
+                                <img src={cart} alt='Cart icon' width={25} height={25} />
                                 <span>
                                 <span>Basket</span>
                                 <span>Empty</span>
                               </span>
                             </Link>
                             <button className='section-nav-right-menu' onClick={() => this.setState({ open_right_bar: !open_right_bar })}>
-                                <img src={menu} alt='Menu icon' />
+                                <img src={menu} alt='Menu icon' width={25} />
                             </button>
                         </div>
                     </div>
