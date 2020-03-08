9bce4f3097f38727329e0802fd27826c65419333
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-03-08 12:24:26 +0500

    Update help.js

--- src/pages/help.js
+++ src/pages/help.js
@@ -6,7 +6,7 @@ import { Wrapper, Hero, ImpossibleBanner} from "../components";
 export default class Help extends React.Component {
     render() {
         return (
-            <Wrapper name='Returns' location={this.props.location} gymNav={true}>
+            <Wrapper name='Help' location={this.props.location} gymNav={true}>
                 <Hero>
                     <div className='section-shipping-hero'>
                         <h1>Help</h1>
