ed968a9f69b93157b7a0d1ef61b73c0e3ac1f731
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-03-10 14:38:55 +0500

    Loading: left side animation completed

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -3,8 +3,6 @@ import React from 'react'
 import '../styles/loading.css'
 import logo_light from "../images/logo_light.png";
 
-const height = window.innerHeight;
-
 export default class Loading extends React.Component {
     state = {
       loaded: 0,
@@ -50,7 +48,12 @@ export default class Loading extends React.Component {
                     ))}
                 </div>
                 <div className='space-between section-loading-main-content'>
-                    <p className='section-loading-left-slogan'>Find & release your inner athlete</p>
+                    {loaded > 20 &&
+                        <div className='section-loading-left-slogan-container'>
+                            <span className='section-loading-left-slogan'>Find & release your inner athlete</span>
+                            <div className='section-loading-left-slogan-cover' />
+                        </div>
+                    }
                     <div className='section-loading-minor-headings'>
                         <div className='section-loading-top-minor-heading-container'>
                             <h2 className='section-loading-minor-heading'>{loaded}%</h2>
@@ -61,10 +64,12 @@ export default class Loading extends React.Component {
                     <div className='align-center section-loading-logo-container'>
                         <img src={logo_light} alt='logo_light' className='section-loading-logo'/>
                     </div>
-                    <div className='space-between section-loading-right-slogan'>
-                        <p>+</p>
-                        <p className=''>Find & release your inner athlete</p>
-                    </div>
+                    {loaded > 20 &&
+                        <div className='space-between section-loading-right-slogan'>
+                            <p>+</p>
+                            <p className=''>Find & release your inner athlete</p>
+                        </div>
+                    }
                 </div>
             </div>
 
