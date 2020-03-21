c151c646e7c5ab949cdd166019f7eabcbeaf46dc
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-03-22 00:10:03 +0500

    Login: remove data fetching effect fn.

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import gql from "graphql-tag";
 import { useMutation } from '@apollo/react-hooks';
 
@@ -23,20 +23,28 @@ export default () => {
     const [email, setEmail] = useState("");
     const [loading, setLoading] = useState(false);
     const [password, setPassword] = useState("");
-    const [login, { data }] = useMutation(LOGIN_MUTATION);
+    const [login] = useMutation(LOGIN_MUTATION);
     const enable = password !== '' && email !== '';
-    const didMountRef = useRef(false);
 
     const handleKeyPress = useCallback( (e) => {
-        e.preventDefault();
         if (e.key === 'Enter') {
+            e.preventDefault()
             handleLogin()
         }
     }, [email, password]);
 
-    const handleLogin = async () => {
+    const handleLogin =  () => {
+
         setLoading(true);
-        await login({ variables: { input: {identifier: email, password: password}}})
+        login({ variables: { input: {identifier: email, password: password}}}).then((res) => {
+            if (res.data.login) {
+                setLoading(false);
+                console.log(res.data.login, loading)
+            }
+        }).catch(error => {
+            console.log(error);
+            setLoading(false);
+        })
     };
 
     useEffect(() => {
@@ -46,19 +54,9 @@ export default () => {
         };
     }, [handleKeyPress]);
 
-    useEffect(() => {
-        if (didMountRef.current) {
-            if (data.login) {
-                setLoading(false);
-                console.log(data.login, loading)
-            }
-        } else didMountRef.current = true
-    }, [data]);
-
-
     return (
         <div className='column-center' style={{width: '30%', margin: 'auto', height: '100vh', justifyContent: 'center'}}>
-            <BasketInput className='section-basket-address-input' title='Email' name='email' width={100} onChange={(e) => setEmail(e.target.value)} value={email} />
+            <BasketInput className='section-basket-address-input' title='Username / Email' name='email' width={100} onChange={(e) => setEmail(e.target.value)} value={email} />
             <BasketInput type='password' className='section-basket-address-input' title='Password' name='password' width={100} onChange={(e) => setPassword(e.target.value)} value={password} />
             <button
                 disabled={!enable}
