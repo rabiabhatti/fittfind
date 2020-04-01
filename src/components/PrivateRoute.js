0ee77c3d3b384c3c011673a2fc9bd398314820dc
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-04-01 11:23:52 +0500

    Check for user instead of token in Private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react"
 import { navigate } from "gatsby"
+import { useSelector, shallowEqual } from "react-redux";
 
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
-    const token = localStorage.getItem('token');
-    if (token === '' && location.pathname !== `/app/login`) {
+    const { user } = useSelector(state => ({
+        user: state.auth.user
+    }), shallowEqual);
+
+    if (!user && location.pathname !== `/app/login`) {
          navigate("/app/login");
         return null
     }
     return <Component {...rest} />
-}
+};
+
 export {PrivateRoute}
