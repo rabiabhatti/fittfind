f9954e7a3c8fd8dbe5b209e8e1cb5f593e9f11ee
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-06-08 01:27:41 +0500

    Common: all categories list added

--- common.js
+++ common.js
@@ -1,4 +1,59 @@
+import default_banner from "./src/images/default_category_banner.png";
+
 export const STRAPI_SERVER_URL = 'http://localhost:1337';
 export const ECWID_URL = 'https://app.ecwid.com/api/v3/27677024';
 export const ECWID_TOKEN = 'secret_TyZ2wNXHhsuQxPrNfjzdVm1k8pL5Ra2H';
 
+export const allCategories = [
+    {
+        name: 'New Release',
+        id: '49223333',
+        gender: 'women',
+        banner: default_banner,
+    },{
+        name: 'Sport Bras',
+        id: '49258289',
+        gender: 'women',
+        banner: default_banner,
+    },{
+        name: 'Tank tops',
+        id: '49259352',
+        gender: 'women',
+        banner: default_banner,
+    },{
+        name: 'Leggings',
+        id: '49258291',
+        gender: 'women',
+        banner: default_banner,
+    },{
+        name: 'Shorts',
+        id: '49225598',
+        gender: 'women',
+        banner: default_banner,
+    },{
+        name: 'New Release',
+        id: '49259349',
+        gender: 'men',
+        banner: default_banner,
+    },{
+        name: 'Compression Tops',
+        id: '49259351',
+        gender: 'men',
+        banner: default_banner,
+    },{
+        name: 'Tights',
+        id: '49225592',
+        gender: 'men',
+        banner: default_banner,
+    },{
+        name: 'Hoodies',
+        id: '49225597',
+        gender: 'men',
+        banner: default_banner,
+    },{
+        name: 'Joggers and sweatpants',
+        id: '49225597',
+        banner: default_banner,
+        gender: 'men',
+    },
+]
