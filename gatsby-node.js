df3a3d39a4ed14cf1fcde9c01c8df34be7e9c961
Rabia <rabia.iqbal.bhatti@gmail.com>
2020-06-07 05:02:31 +0500

    gatsby-node.js: remove strapi dependent pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,76 +4,74 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+
 const path = require(`path`);
 
-const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
-    resolve(
-        graphql(request).then(result => {
-            if (result.errors) {
-                reject(result.errors)
-            }
+const categories = [
+    {
+        name: 'Tank tops',
+        id: '49259352',
+        gender: 'women',
+        img: 'https://dpbfm6h358sh7.cloudfront.net/images/27677024/1394034243.jpg',
+    },{
+        name: 'Leggings',
+        id: '49258291',
+        gender: 'women',
+        img: 'https://dpbfm6h358sh7.cloudfront.net/images/27677024/1394035327.jpg',
+    },{
+        name: 'Sport Bras',
+        id: '49258289',
+        gender: 'women',
+        img: 'https://dpbfm6h358sh7.cloudfront.net/images/27677024/1394035026.jpg'
+    },{
+        name: 'New Release',
+        id: '49223333',
+        gender: 'women',
+        img: 'https://dpbfm6h358sh7.cloudfront.net/images/27677024/1393747622.jpg'
+    },{
+        name: 'Shorts',
+        id: '49225598',
+        gender: 'women',
+    },{
+        name: 'Compression Tops',
+        id: '49259351',
+        gender: 'men',
+        img: 'https://dpbfm6h358sh7.cloudfront.net/images/27677024/1394024833.jpg',
+    },{
+        name: 'New Release',
+        id: '49259349',
+        gender: 'men',
+        img: 'https://dpbfm6h358sh7.cloudfront.net/images/27677024/1394024570.jpg',
+    },{
+        name: 'Hoodies',
+        id: '49225597',
+        gender: 'men',
+    },{
+        name: 'Tights',
+        id: '49225592',
+        gender: 'men',
+        img: 'https://dpbfm6h358sh7.cloudfront.net/images/27677024/1394023347.jpg',
+    },{
+        name: 'Joggers and sweatpants',
+        id: '49225597',
+        img: '',
+        gender: 'men',
+    },
+]
 
-            return result;
-        })
-    )
-});
-
-exports.createPages = ({ actions, graphql }) => {
+exports.createPages = ({ actions }) => {
     const { createPage } = actions;
 
-    const getProducts = makeRequest(graphql, `
-    {
-      allStrapiProduct {
-        edges {
-          node {
-            id
-            gender {
-                type
-            }
-          }
-        }
-      }
-    }
-    `).then(result => {
-        result.data.allStrapiProduct.edges.forEach(({ node }) => {
-            createPage({
-                path: `/product_id=${node.id.split('_')[1]}`,
-                component: path.resolve(`src/templates/product.js`),
-                context: {
-                    id: node.id,
-                },
-            })
+    categories.forEach(category => {
+        createPage({
+            path: `/${category.gender}/${category.name.split(' ').join('-').toLowerCase()}`,
+            component: path.resolve(`src/templates/category.js`),
+            context: {
+                category: category.name,
+                gender: category.gender,
+                id: category.id,
+                img: category.img,
+            },
         })
-    });
-
-    const getCategories = makeRequest(graphql, `
-        {
-          allStrapiCategory {
-            edges {
-              node {
-                name
-                gender {
-                  type
-                }
-              }
-            }
-          }
-        }
-    `).then(result => {
-        result.data.allStrapiCategory.edges.forEach(({ node }) => {
-            createPage({
-                path: `/${node.gender.type}/${node.name.split(' ').slice(1).join('-').toLowerCase()}`,
-                component: path.resolve(`src/templates/category.js`),
-                context: {
-                    category: node.name,
-                    gender: node.gender.type
-                },
-            })
-        })
-    });
-
-    return Promise.all([
-        getCategories,
-        getProducts,
-    ])
+    })
 };
